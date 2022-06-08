734489667f30f183d39e6299a5a5b7b21bb241dc
asengupt <asengupt@progress.com>
2022-06-08 18:43:36 -0400

    initial

---
+++ test/src/main/java/test/TestMain.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.io.File;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		File dir = new File(System.getProperty("user.dir"));
+		for(File f:dir.listFiles()) {
+			System.out.println(f.getName());
+		}
+		
+
+	}
+
+}
